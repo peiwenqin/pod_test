41d850408c9f00eeaf1667b7dd40623e26325aad
peiwenqin <wenqin.pei@wowjoy.cn>
2020-07-04 16:59:13 +0800

    第一次提交

---
+++ Example/pod_test/CaculatorTool.swift
@@ -0,0 +1,15 @@
+//
+//  CaculatorTool.swift
+//  pod_test_Example
+//
+//  Created by pei on 2020/7/4.
+//  Copyright © 2020 CocoaPods. All rights reserved.
+//
+
+import UIKit
+
+class CaculatorTool: NSObject {
+    static func sum(a: NSInteger, b: NSInteger) -> NSInteger{
+        return a+b;
+    }
+}
